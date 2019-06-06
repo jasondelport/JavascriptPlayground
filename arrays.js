e7652aa57732797c506286a61f06b7e2fbcd9aaf
Jason Delport <jason.delport@keytree.co.uk>
2019-06-06 09:09:50 +0100

    First commit

---
+++ arrays.js
@@ -0,0 +1,46 @@
+[1,2,3].forEach(function(item, index, array) {
+	console.log(item, index, array);
+});
+
+[1,2,3].forEach(function(item) {
+	console.log(item);
+});
+
+[1,2,3].forEach(function(item) {
+	console.log(item, this);
+}, [1,2,3]);
+
+const result = [1,2,3].map(function(item) {
+	return item * 2;
+});
+console.log(result);
+
+const evens = [1,2,3,4,5,6].filter(function(item) {
+	return item % 2 ===0;
+});
+console.log(evens);
+
+const sum = [1,2,3,4,5,6].reduce(function(result, item) {
+	return result + item;
+}, 5); // this is the starting value
+console.log(sum);
+
+const hasNeg = [1,2,3,4,-1,6].some(function(item) {
+	return item < 0;
+});
+console.log(hasNeg);
+
+const allPos = [1,2,3,4,-1,6].every(function(item) {
+	return item > 0;
+});
+console.log(allPos);
+
+const foundObject = [{id:'1'},{id:'2'},{id:'3'}].find(function(item) {
+	return item.id === '2';
+});
+console.log(JSON.stringify(foundObject));
+
+const foundIndex = [{id:'1'},{id:'2'},{id:'3'}].findIndex(function(item) {
+	return item.id === '2';
+});
+console.log(JSON.stringify(foundIndex));

3878d56d2dad4903a63020cab380a2549906dd83
Jason Delport <jason.delport@keytree.co.uk>
2019-06-21 15:32:18 +0100

    More collection object examples

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
+//https://medium.com/siliconwat/data-structures-in-javascript-1b9aed0ea17c
+// https://javascript.info/map-set-weakmap-weakset
+
 const array = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < array.length; i++) {
@@ -19,6 +22,10 @@ for (let key in object) {
 	console.log(key + " -> " + object[key]);
 }
 
+for (let [key, value] of Object.entries(object)) {
+ console.log(`${key} -> ${value}`);
+}
+
 [1, 2, 3].forEach(function(item, index, array) {
 	console.log(item, index, array);
 });
@@ -32,14 +39,14 @@ for (let key in object) {
 }, [1, 2, 3]);
 
 const result = [1, 2, 3].map(function(item) {
-	return item * 2;
+	return item * 2; // return value
 });
 console.log(result);
 
 const evens = [1, 2, 3, 4, 5, 6].filter(function(item) {
 	return item % 2 === 0;
 });
-console.log(evens);
+console.log("filter/evens -> " + evens);
 
 const evens1 = [1, 2, 3, 4, 5, 6].filter(item => item % 2 === 0);
 console.log(evens1);
@@ -49,10 +56,10 @@ const sum = [1, 2, 3, 4, 5, 6].reduce(function(result, item) {
 }, 5); // this is the starting value
 console.log(sum);
 
-const uniqueArray = [1, 2, 2, 3, 4, 4, 5, 5, 5, 6].filter((item, pos, ar) => {
-	// console.log(item, pos, ar);
+const uniqueArray = [1, 2, 2, 3, 4, 4, 5, 5, 5, 6].filter((item, position, array) => {
+	console.log(item, position, array, array.indexOf(item) === position);
 	// indexOf() method returns the first index at which a given element can be found in the array
-	return ar.indexOf(item) === pos;
+	return array.indexOf(item) === position; // returns trues
 });
 console.log("unique array -> " + uniqueArray);
 
@@ -87,3 +94,37 @@ const foundIndex = [{
 	return item.id === '2';
 });
 console.log(JSON.stringify(foundIndex));
+
+var map = new Map();
+map.set("0", "foo");
+map.set(1, "bar");
+map.set({}, "baz");
+
+for (const entry of map.entries()) {
+  console.log(entry);
+}
+
+map.forEach(function(value, key, map) {
+	console.log(`${key} -> ${value}`)
+});
+
+for (const entry of map) {
+  console.log(entry);
+}
+
+
+for (let key of map.keys()) {
+	console.log(key);
+}
+
+for (let value of map.values()) {
+	console.log(value);
+}
+
+
+
+
+
+
+
+
